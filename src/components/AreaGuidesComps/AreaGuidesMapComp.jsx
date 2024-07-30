754013a0e3d833ca7d651530d8ab9774034d9a77
Asim Shakoor <asimshakoor920830@gmail.com>
2024-07-31 02:10:38 +0500

    changes

--- src/components/AreaGuidesComps/AreaGuidesMapComp.jsx
+++ src/components/AreaGuidesComps/AreaGuidesMapComp.jsx
@@ -2135,20 +2135,20 @@ const AreaGuidesMapComp = ({ customview }) => {
 
       <button
         onClick={handleReset}
-        className="absolute top-4 left-4 bg-primarycolor hover:bg-primarycolorhover text-white font-medium px-4 py-2 rounded-lg cursor-pointer transition-colors duration-300 ease-in-out"
+        className="absolute top-8 md:top-4 left-4 bg-primarycolor hover:bg-primarycolorhover text-white font-medium px-4 py-2 rounded-lg cursor-pointer transition-colors duration-300 ease-in-out"
       >
         Show All Markers
       </button>
 
       {selectedLocation && (
-        <div className="absolute bottom-4 right-4 bg-white rounded-lg flex items-start">
+        <div className="absolute bottom-8 md:bottom-4 right-4 bg-white rounded-lg flex items-start">
           <div className='relative overflow-hidden bg-container rounded-tl-lg rounded-bl-lg'>
             <img src={selectedLocation.info.image} alt={selectedLocation.info.title} className="w-40 h-32 bg-zoom object-cover rounded-tl-lg rounded-bl-lg" />
 
           </div>
-          <div className="px-6 py-4">
-            <h3 className="text-base font-semibold">{selectedLocation.info.title}</h3>
-            <p className='text-sm mt-5'>View Area / View Properties</p>
+          <div className="p-3 md:px-6 md:py-4">
+            <h3 className="text-sm md:text-base font-semibold">{selectedLocation.info.title}</h3>
+            <p className='text:xs md:text-sm mt-5'>View Area / View Properties</p>
           </div>
         </div>
       )}
